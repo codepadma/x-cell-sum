05ff2af2f57522af2bc72baa348dc2c7d692bd6a
codepadma <padma.govindarajalu@gmail.com>
2017-04-29 14:47:04 -0700

    add code to render table body

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 const TableModel = require('./table-model');
 const TableView = require('./table-view');
 
-const model = new TableModel();
+const model = new TableModel(5, 10);
 const view = new TableView(model);
 view.init();
--- client/js/table-view.js
+++ client/js/table-view.js
@@ -1,4 +1,4 @@
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTH, createTR, createTD } = require('./dom-util');
 const { getLetterRange } = require('./array-util');
 
 class TableView {
@@ -13,11 +13,12 @@ class TableView {
 
   initDomReferences() {
   	this.headerRowEle = document.querySelector('THEAD TR');
+    this.bodyEle = document.querySelector('TBODY');
   }
 
   renderTable() {
   	this.renderTableHeader();
-  	//this.renderTableBody();
+  	this.renderTableBody();
   }
 
   renderTableHeader() {
@@ -25,6 +26,20 @@ class TableView {
     getLetterRange('A', this.model.numOfCols)
       .map(colLabel => createTH(colLabel))
       .forEach(th => this.headerRowEle.appendChild(th));
+  }
+
+  renderTableBody() {
+    const docFragment = document.createDocumentFragment();
+    for(let row = 0; row < this.model.numOfRows; row++) {
+      const tr = createTR();
+      for(let col = 0; col < this.model.numOfCols; col++) {
+        const td = createTD();
+        tr.appendChild(td);
+      }
+      docFragment.appendChild(tr);
+    }
+    removeChildren(this.bodyEle);
+    this.bodyEle.appendChild(docFragment);
   }
 }
 
