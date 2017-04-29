5d7e2f65d027b097e58b71611075043297b13b88
codepadma <padma.govindarajalu@gmail.com>
2017-04-29 08:31:25 -0700

    add dom util tests

--- client/js/test/dom-util.test.js
+++ client/js/test/dom-util.test.js
@@ -1,18 +1,66 @@
-const { removeChildren } = require('../dom-util');
+const { removeChildren,
+        createTR,
+        createTH,
+        createTD } = require('../dom-util');
 
 describe('dom-util', () => {
+
+  describe('DOM create functions', () => {
+  	describe('createTH', () => {
+      it('produces valid TH element', () => {
+        const ele = createTH();
+        expect(ele.tagName).toBe('TH');
+      });
+
+      it('sets the text of TH element', () => {
+      	const text = 'Table Header';
+      	const ele = createTH(text);
+      	expect(ele.textContent).toBe(text);
+      });
+    });
+
+    describe('createTR', () => {
+      it('produces valid TR element', () => {
+        const ele = createTR();
+        expect(ele.tagName).toBe('TR');
+      });
+
+      it('sets the text of TR element', () => {
+      	const text = 'Table Row';
+      	const ele = createTR(text);
+      	expect(ele.textContent).toBe(text);
+      });
+    });
+
+    describe('createTD', () => {
+      it('produces valid TD element', () => {
+        const ele = createTD();
+        expect(ele.tagName).toBe('TD');
+      });
+
+      it('sets the text of TD element', () => {
+      	const text = 'Table Data';
+      	const ele = createTD(text);
+      	expect(ele.textContent).toBe(text);
+      });
+    });
+
+  });
   
-  it('removes one child', () => {
-    const parent = document.createElement('DIV');
-    const child = document.createElement('STRONG');
+  describe('remove children', () => {
+  
+    it('removes one child', () => {
+      const parent = document.createElement('DIV');
+      const child = document.createElement('STRONG');
 
-    parent.appendChild(child);
+      parent.appendChild(child);
 
-    expect(parent.childNodes.length).toBe(1);
-    expect(parent.childNodes[0]).toBe(child);
+      expect(parent.childNodes.length).toBe(1);
+      expect(parent.childNodes[0]).toBe(child);
 
-    removeChildren(parent);
-    
-    expect(parent.childNodes.length).toBe(0);
+      removeChildren(parent);
+
+      expect(parent.childNodes.length).toBe(0);
+    });
   });
 });
